6f71a5a2ffa8f6544db8ffb62ea6a6dec647e26a
Харченко Владислав Дмитриевич <vladislavharcenko14@gmail.com>
2020-02-22 19:56:47 +0300

    [WEB]: Added: drag-and-drop feature. Needs: send file in js.

--- static/js/photo.js
+++ static/js/photo.js
@@ -1,40 +1,50 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    let dropZone = document.querySelector("#photo-frame-in-content-group");
+    let dropArea = document.getElementById('photo-frame-in-content-group');
 
-    dropZone.addEventListener("drag dragstart dragend dragover dragenter dragleave drop", function() {
-        return false;
+    ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, preventDefaults, false);
     });
 
-    dropZone.addEventListener("dragover dragenter", function() {
-        dropZone.classList.add('dragover');  // fix it
+    function preventDefaults (e) {
+        e.preventDefault();
+        e.stopPropagation();
+    }
+
+    ['dragenter', 'dragover'].forEach(eventName => {
+        dropArea.addEventListener(eventName, highlight, false);
     });
 
-    dropZone.addEventListener("dragleave", function(e){
-        let dx = e.pageX - dropZone.offset.left;  //fix it
-        let dy = e.pageY - dropZone.offset.top;
-        if ((dx < 0) || (dx > dropZone.width()) || (dy < 0) || (dy > dropZone.height())) {
-            dropZone.classList.remove('dragover');
-        }
+    ['dragleave', 'drop'].forEach(eventName => {
+        dropArea.addEventListener(eventName, unhighlight, false);
     });
 
-    dropZone.addEventListener('drop', function(e) {
-        dropZone.classList.remove('dragover');
-		let files = e.originalEvent.dataTransfer.files;
-		sendFiles(files);
-    });
+    function highlight(e) {
+        dropArea.classList.add('highlight');
+    }
 
-    document.querySelector('#file-input').change(function(){
-        let files = this.files;
-		sendFiles(files);
-    });
+    function unhighlight(e) {
+        dropArea.classList.remove('highlight');
+    }
 
-    function sendFiles(files) {  //fix it
-		let maxFileSize = 5242880;
-		let Data = new FormData();
-		Array.prototype.forEach.call(document.querySelectorAll(files), function(index, file){
-			if ((file.size <= maxFileSize) && ((file.type == 'image/png') || (file.type == 'image/jpeg'))) {
-				Data.append('images[]', file);
-			}
-	    });
-	}
+    dropArea.addEventListener('drop', handleDrop, false);
+
+    function handleDrop(e) {
+        let dt = e.dataTransfer;
+        let files = dt.files;
+
+        handleFiles(files);
+    }
+
+    function handleFiles(files) {
+        ([...files]).forEach(uploadFile);
+    }
+
+    function uploadFile(file) {
+        let formData = new FormData();
+        formData.append('file', file);
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        });
+    }
 });
