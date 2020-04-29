7d6e0b15ee5f94853ff615690ec5b8c80144ed84
Харченко Владислав Дмитриевич <vladislavharcenko14@gmail.com>
2020-04-29 19:06:39 +0300

    [WEB]: prepare

---
+++ static/js/profile.js
@@ -0,0 +1,6 @@
+while (document.getElementById('rating').getElementsByClassName('active').length > 1) {
+    document.getElementById('rating').getElementsByClassName('active')[1].classList.remove('active');
+}
+while (document.getElementById('goods').getElementsByClassName('active').length > 1) {
+    document.getElementById('goods').getElementsByClassName('active')[1].classList.remove('active');
+}
