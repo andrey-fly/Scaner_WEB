45d41cb6ef4ad0dbd2d72da5a8d9ced0c1f45be2
Ender <sim200347@gmail.com>
2020-05-13 21:44:09 +0300

    [WEB]: maker more dashboards

--- static/js/profile.js
+++ static/js/profile.js
@@ -4,3 +4,12 @@ while (document.getElementById('rating').getElementsByClassName('active').length
 while (document.getElementById('goods').getElementsByClassName('active').length > 1) {
     document.getElementById('goods').getElementsByClassName('active')[1].classList.remove('active');
 }
+
+function preview_image(event) {
+    var reader = new FileReader();
+    reader.onload = function() {
+        var output = document.getElementById('output_image');
+        output.src = reader.result;
+    }
+    reader.readAsDataURL(event.target.files[0]);
+}
