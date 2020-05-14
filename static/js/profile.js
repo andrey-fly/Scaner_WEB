8396a52625ebe77449429d9816741cc2c23ed34e
Ender <sim200347@gmail.com>
2020-05-14 23:05:44 +0300

    [WEB]: fix modal

--- static/js/profile.js
+++ static/js/profile.js
@@ -4,10 +4,9 @@ while (document.getElementById('rating').getElementsByClassName('active').length
 while (document.getElementById('goods').getElementsByClassName('active').length > 1) {
     document.getElementById('goods').getElementsByClassName('active')[1].classList.remove('active');
 }
-
 function preview_image(event) {
-    var reader = new FileReader();
-    reader.onload = function() {
+        var reader = new FileReader();
+        reader.onload = function() {
         var output = document.getElementById('output_image');
         output.src = reader.result;
     }
